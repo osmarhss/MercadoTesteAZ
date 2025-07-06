26dc80afef80796a6993bf7af14a818726150c2c
osmar <osmarhenriquecn@hotmail.com>
2025-07-06 20:16:33 -0300

    Retirado o construtor da classe PayPal

--- MercadoTesteAZ/Domínio/MeiosDePagamento/PayPal.cs
+++ MercadoTesteAZ/Domínio/MeiosDePagamento/PayPal.cs
@@ -5,15 +5,6 @@ namespace MercadoTesteAZ.Models.MeiosDePagamento
 {
     public class PayPal : IMetodosPagamento, IEntity
     {
-        private PayPal(){}
-        public PayPal(string id, string email, string nomeTitular, string clienteId)
-        {
-            Id = string.IsNullOrWhiteSpace(id) ? Guid.NewGuid().ToString("D") : id;
-            Email = email;
-            NomeTitular = nomeTitular;
-            ClienteId = clienteId;
-        }
-
         public string Id { get; private set; }
         public string Email { get; private set; }
         public string NomeTitular { get; private set; }
